ffcb22c0713597199f5b9e5cd86625e4ba90c01e
Simon Melchertsen <warcraft.simon@gmail.com>
2019-11-07 09:42:53 +0100

    Update TestData.cs

--- MovieRatingLibrary/Data/TestData.cs
+++ MovieRatingLibrary/Data/TestData.cs
@@ -7,7 +7,6 @@ namespace MovieRatingLibrary
     public class TestData
     {
         public List<MovieReview> MovieReviews { get; set; }
-adasdasdasdasdsa
         public TestData()
         {
             InitializeTestData();
